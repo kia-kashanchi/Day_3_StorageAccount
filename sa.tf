8943ee7e4fd3c6deefe0ae98367db49a4199107b
kia-kashanchi <kia.kashanchi@sanofi.com>
2025-05-19 11:47:36 +0200

    Initial deployment

--- sa.tf
+++ sa.tf
@@ -1,10 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.29.0"
+    }
+  }
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "tf-backend"
   location = "Spain Central"
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "kiatechtutorial-day3-tfbackend"
+  name                     = "kiatechtutorialday3back"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
