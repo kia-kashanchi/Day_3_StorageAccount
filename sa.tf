fadb108668e76d1c26f265d515484d249b8406cc
kia-kashanchi <kia.kashanchi@sanofi.com>
2025-05-19 11:55:49 +0200

    Changed replication to LRS

--- sa.tf
+++ sa.tf
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
   account_kind = "BlobStorage"
   tags = {
     environment = "development"
