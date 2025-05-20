30f68896795822ee1e68a4719ffafbe684a92ea6
kia-kashanchi <kia.kashanchi@sanofi.com>
2025-05-20 15:04:34 +0200

    Modified SA name and added tfplan as GH artifact

--- sa.tf
+++ sa.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "kiatechtutorialday3back"
+  name                     = "kiatechtutorialbackend"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
